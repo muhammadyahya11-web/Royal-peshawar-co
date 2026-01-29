12cfd4d8e1797eeca62a891ea6c7764ea5922914
Muhammadyahya11-web <muhammadyahya50501@gmail.com>
2026-01-29 02:47:36 -0800

    error fix

--- admin-Penal/vite.config.js
+++ admin-Penal/vite.config.js
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
-// https://vite.dev/config/
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss(),],
-   server: {
+  plugins: [react()],
+  server: {
     host: true,
-    port: 5175,   
+    port: 5175,
+  },
+  build: {
+    outDir: 'dist', // ensure Vercel knows the output folder
   },
 })
-
-
