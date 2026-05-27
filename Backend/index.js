c4647a84db2f7faad7fb5cf579889316f938ee3b
Muhammadyahya11-web <muhammadyahya50501@gmail.com>
2026-05-27 13:05:28 -0700

    Updated deprecated packages

--- Backend/index.js
+++ Backend/index.js
@@ -29,7 +29,7 @@ app.use(
   cors({
     origin: [
       process.env.FRONTEND_URL || "http://localhost:5173",
-      "http://localhost:5174",
+      "https://royal-peshawar-co.vercel.app",
     ],
     credentials: true,
   })
