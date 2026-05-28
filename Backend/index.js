d34b3f9e4dbbfaac8476c298a0640066390fbc85
Muhammadyahya11-web <muhammadyahya50501@gmail.com>
2026-05-28 13:04:34 -0700

    fix folder case issue Config -> config

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-import connectDb from "./Config/Db.js";
+import connectDb from "./config/Db.js";
 
 import authRoutes from "./routes/AuthRoutes.js";
 import userRoutes from "./routes/UserRoutes.js";
