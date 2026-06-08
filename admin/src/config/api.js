6624449045818ee6179f8f898b3351fe0a90c12f
Muhammadyahya11-web <muhammadyahya50501@gmail.com>
2026-06-08 13:35:20 -0700

    erorr

--- admin/src/config/api.js
+++ admin/src/config/api.js
@@ -1,2 +1,2 @@
 export const API_BASE =
-  import.meta.env.VITE_API_URL || "https://royal-peshawar-co-server.vercel.app";
+  import.meta.env.VITE_API_URL || "https://royal-peshawar-co-server.vercel.app/api";
