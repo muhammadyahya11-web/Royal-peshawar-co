1f2a10c087ecfb3b7294411848c596f0f7a3cec3
Muhammadyahya11-web <muhammadyahya50501@gmail.com>
2026-06-08 11:47:04 -0700

    fix error

--- Backend/index.js
+++ Backend/index.js
@@ -38,8 +38,9 @@ app.post(
 app.use(
   cors({
     origin: [
-      process.env.FRONTEND_URL || "http://localhost:5173" ,
-      "https://royal-peshawar-co.vercel.app/", "http://localhost:5174" ,
+      "https://royal-peshawar-co.vercel.app/" || "http://localhost:5173" ,
+      , "http://localhost:5174" ,
+      
     ],
     credentials: true,
   })
